66e035c3d958ff6ded8daf6152d4eef1e05d2f20
smartquail <smartquail@MacBook-Air-de-smartquail.local>
2022-12-12 12:13:58 -0500

    test

--- qnd00_app_prod/manage.py
+++ qnd00_app_prod/manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd00_app_prod.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd00_app_prod.settings.pro')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,4 +20,6 @@ def main():
 
 
 if __name__ == '__main__':
+
+    load_dotenv()
     main()
